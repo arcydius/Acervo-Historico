84cc271e036934465778c2447ae321f5db8469ac
Ricardo <arcidiomp03@gmail.com>
2026-07-18 14:22:28 -0400

    Eliminado boton de alto contraste

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Lógica para el botón de Alto Contraste
-const btnContraste = document.getElementById('btn-contraste');
-
-if (btnContraste) {
-    btnContraste.addEventListener('click', () => {
-        document.body.classList.toggle('alto-contraste');
-        
-        if (document.body.classList.contains('alto-contraste')) {
-            btnContraste.textContent = 'Modo Normal';
-        } else {
-            btnContraste.textContent = 'Alto Contraste';
-        }
-    });
-}
-
 // Lógica para la barra de navegación pegajosa (Sticky/Fixed) con fondo negro al hacer scroll
 const navbar = document.querySelector('.navbar');
 
